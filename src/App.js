452215c59ab8cc20ae397268883f66083334b139
Daniel Banach <djbanach@comcast.net>
2019-04-24 14:53:25 -0700

    added axios request for users and table to store data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
-import React from 'react';
-import './App.css';
-import { Form } from "./components/Form";
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Table } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Form />
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+      pictures: []
+    }
+  }
+
+  componentDidMount() {
+      axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const user = res.data;
+        this.setState({
+          isLoaded: true,
+          items: user
+        });
+      })
+
+      // axios.get(`https://jsonplaceholder.typicode.com/photos`)
+      // .then(res => {
+      //   const
+      // })
+  }
+
+  render() {
+    var { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>
+    }
+    else {
+      return (
+        <div className="App">
+          <Table striped bordered hover>
+            <thead>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone Number</th>
+            </thead>
+            <tbody>
+              {items.map(item => (
+                <tr key={item.id}>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phone}</td>
+                </tr>
+              ))}
+
+            </tbody>
+          </Table>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
